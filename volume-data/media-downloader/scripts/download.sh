87dbbd94ba4db39f28112379f1a1248bf191a68f
Jason Rogena <null+github.com@rogena.me>
2025-05-14 16:35:27 +0000

    Update media downloader containers
    Tweak the media downloading containers to avoid having to wait for
    too long for content to be downloaded from put.io. Also improve the
    reliability of the downloading since we don't need to create several
    tmp dirs which are hard to track.
    
    Signed-off-by: Jason Rogena <null+github.com@rogena.me>

--- volume-data/media-downloader/scripts/download.sh
+++ volume-data/media-downloader/scripts/download.sh
@@ -1,11 +1,8 @@
 #! /usr/bin/env bash
 
 download_from_folder() {
-  tmpdir=$(/usr/bin/mktemp -d -p /mnt/media/tmp)
-  /usr/bin/putio "$PUTIO_OAUTH_TOKEN" "$1" 5 | /usr/bin/xargs /usr/bin/aria2c -d "$tmpdir" -x 5 --auto-file-renaming=false --allow-overwrite=false
-  /usr/bin/putio "$PUTIO_OAUTH_TOKEN" "$1" 5
-  /usr/bin/mv "$tmpdir"/* /mnt/media/downloads/
-  /usr/bin/rm -r "$tmpdir"
+  /usr/bin/putio "$PUTIO_OAUTH_TOKEN" "$1" 5 5 | /usr/bin/xargs /usr/bin/aria2c -x 5 --auto-file-renaming=false --allow-overwrite=false
+  /usr/bin/putio "$PUTIO_OAUTH_TOKEN" "$1" 5 5
   sleep_sec=120
   echo "Sleeping for $sleep_sec seconds"
   sleep "$sleep_sec"
